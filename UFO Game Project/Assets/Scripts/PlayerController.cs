5585bdf6d982296768c26b64fe78c1e469a0aa84
AaronCur <c00202505@itcarlow.ie>
2017-10-06 15:13:35 +0100

    Lab 2 complete

---
+++ UFO Game Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿//Name: Aaron Curry
+//Start Date: 
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private float rad;
+    //Compare rad is used to more acurately represent the players size when scaled down vs enemy moates
+    //At scale = 1 the player is much bigger than the enemy players so compare rad is used to compensate.
+    private Rigidbody2D rb2d;
+
+    private int count;
+
+    void Start()
+    {
+        rad = 0.5f;
+        rb2d = GetComponent<Rigidbody2D>();
+        transform.localScale = new Vector3(rad, rad, 0);
+        rb2d.useAutoMass= true;
+        count = 0;
+        SetCountText();
+       
+        winText.text = "";
+    }
+
+	void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+
+
+        if(Input.anyKeyDown)
+        {
+            transform.localScale -= new Vector3(0.02f, 0.02f);
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            if (other.rigidbody.mass <= rb2d.mass)
+            {
+                other.gameObject.SetActive(false);
+                transform.localScale += new Vector3(0.1f, 0.1f);
+                
+                count = count + 1;
+
+                SetCountText();
+            }
+            else
+            {
+                gameObject.SetActive(false);
+                winText.text = "You Lose";
+            }
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >=20)
+        {
+            winText.text = "You Win";
+        }
+    }
+}
---
+++ UFO Game Project/Assets/Scripts/Rotator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+
+    private Rigidbody2D rbpu;
+    private float rad;
+
+    public float rotaterSpeed;
+    void Start()
+    {
+        rbpu = GetComponent<Rigidbody2D>();
+        rbpu.useAutoMass = true;
+        rad = Random.Range(0.2f, 2.0f);
+        transform.localScale = new Vector3(rad, rad, 0);
+        rbpu.useAutoMass = true;
+
+    }
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+
+        float x = Random.Range(-2.0f,2.0f);
+        float y = Random.Range(-2.0f,2.0f);
+        Vector2 movement = new Vector2(x, y);
+        rbpu.AddForce(movement * rotaterSpeed);
+
+    }
+}
